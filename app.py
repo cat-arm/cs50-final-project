3f0b46670b1f8603dbe6a60245410233e8b83d9b
paiigak <66020711+paiigak@users.noreply.github.com>
2024-12-03 23:47:49 +0700

    adjust api for performance and clean code and adjust html file for set the pattern

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 from datetime import timedelta, datetime, timezone
 from flask import Flask, request, jsonify, render_template, session, redirect, url_for
-from flask_session import Session  # Used for session management
+from flask_session import Session
 from sqlalchemy.orm import Session as DBSession
+from sqlalchemy.sql import func
 from dotenv import load_dotenv
 from models import Role, User, Content
-from utils import is_valid_password, is_valid_email, hash_password, check_password
-from database import get_db
+from utils import get_db, is_valid_password, is_valid_email, hash_password, check_password, get_current_user, check_permission, handle_error
 
 
 # Load environment variables from .env file
@@ -21,15 +21,27 @@ Session(app)  # Initialize session management
 @app.route("/")
 def index():
     db: Session = next(get_db())
-    contents = db.query(Content).filter(Content.status != "Inactive").all()
-    contents_list = [{"id": content.id, "quote": content.quote} for content in contents]
-    return render_template("index.html", contents=contents)
+    contents = (
+        db.query(
+            Content.id,
+            Content.quote,
+            Content.status,
+            func.concat(User.first_name, " ", User.last_name).label("posts_by")
+        )
+        .join(User, Content.created_by == User.id)  # Join Content with User based on created_by
+        .filter(Content.status != "Inactive")  # Exclude inactive content
+        .all()
+    )
+    contents_list = [{"id": content.id, "quote": content.quote, "posts_by": content.posts_by} for content in contents]
+    db.close()
+    print(contents_list)
+    return render_template("index.html", contents=contents_list)
 
 @app.route("/session", methods=["GET"])
 def check_session():
-    user_id = session.get("user_id")
-    if user_id:
-        return jsonify({"message": f"User is logged in with ID {user_id}"})
+    user = get_current_user()
+    if user:
+        return jsonify({"message": f"User is logged in with ID {user.id}"})
     return jsonify({"message": "No user is logged in"}), 401
 
 @app.route("/login", methods=["GET", "POST"])
@@ -53,16 +65,17 @@ def login():
 
             # Set session expiry (30 days)
             app.permanent_session_lifetime = timedelta(days=30)
+            print("login successful")
             return redirect(url_for("index"))
         
         return render_template("login.html", error="Invalid credentials.")
-    
     return render_template("login.html")
 
 @app.route("/logout", methods=["POST"])
 def logout():
     # Clear session data
     session.clear()
+    print("logout successful")
     return redirect(url_for("index"))
 
 @app.route("/register", methods=["GET", "POST"])
@@ -81,7 +94,7 @@ def register():
             return render_template("register.html", error="Invalid password format.")
     
         # Check password and confirmation password match
-        if not password == password_confirm:
+        if password != password_confirm:
             return render_template("register.html", error="Passwords do not match.")
 
         # Check required field
@@ -89,8 +102,7 @@ def register():
             return render_template("register.html", error="All fields are required.")
 
         # check valid email
-        valid_email = is_valid_email(email)
-        if not valid_email:
+        if not is_valid_email(email):
             return render_template("register.html", error="Invalid email address.")
     
         #hash password
@@ -123,20 +135,28 @@ def register():
             # Add the user to the session and commit
             db.add(new_user)
             db.commit()
-            return redirect(url_for("login"))
+
+            # Log the user in by storing user ID in the session
+            session.permanent = True
+            session["user_id"] = new_user.id
+
+            # Set session expiry (30 days)
+            app.permanent_session_lifetime = timedelta(days=30)
+            print("Registration successful and logged in")
+            return redirect(url_for("index"))
 
         except Exception as e:
             db.rollback()  # Rollback on error
-            return render_template("error.html", message=f"Error: {str(e)}")
+            return handle_error(f"Error: {str(e)}")
         finally:
             db.close()  # Close the session after the operation
     return render_template("register.html")
 
 @app.route("/content", methods=["POST"])
-def content():
+def create_content():
     # User must be logged in to create content
-    user_id = session.get("user_id")
-    if not user_id:
+    user = get_current_user()
+    if not user:
         return redirect(url_for("login"))
     
     # quote required
@@ -144,235 +164,195 @@ def content():
     if not quote:
         return render_template("index.html", error="Quote is required.")
     
+    if not check_permission(user, "create_own_content"):
+        return handle_error("Unauthorized to create content.")
+    
     db: Session = next(get_db())
     
     try:
-        user = db.query(User).filter(User.id == user_id).first()
-        if not user:
-            return render_template("error.html", message="User not found.")
-        
-        # role need to be authorized
-        role_id = user.role_id
-        role = db.query(Role).filter(Role.id == role_id).first()
-        if not role or role.name != "user" or "create_own_content" not in role.permissions:
-            return render_template("index.html", error="Unauthorized to create content.")
-
         # Create new content
         new_content = Content(
             quote=quote,
             status="Active",
-            created_by=user_id,
+            created_by=user.id,
             created_at=datetime.now(timezone.utc),
             updated_at=datetime.now(timezone.utc),
         )
 
         db.add(new_content)
         db.commit()
+        print("create content successful")
         return redirect(url_for("index"))
             
     except Exception as e:
         db.rollback()  # Rollback on error
-        return render_template("error.html", message=f"Error: {str(e)}")
+        return handle_error(f"Error: {str(e)}")
     finally:
         db.close()  # Close the session after the operation
 
 @app.route("/content/<content_id>", methods=["PATCH"])
 def update_content(content_id):
     # User must be logged in to create content
-    user_id = session.get("user_id")
-    if not user_id:
+    user = get_current_user()
+    if not user:
         return redirect(url_for("login"))
-    
-    # quote required
-    new_quote = request.form.get("quote", "").strip()
-    if not new_quote:
-        return render_template("index.html", error="Quote is required.")
     
     db: Session = next(get_db())
     
     try:
-        user = db.query(User).filter(User.id == user_id).first()
-        if not user:
-            return render_template("error.html", message="User not found.")
+        # Fetch the content by ID
+        content = db.query(Content).filter(Content.id == content_id).first()
+        if not content:
+            return handle_error("Content not found.")
         
-        # role need to be authorized
-        role_id = user.role_id
-        role = db.query(Role).filter(Role.id == role_id).first()
-        if not role or role.name != "user" or "update_own_content" not in role.permissions:
-            return render_template("index.html", error="Unauthorized to update content.")
+        # Check if the user is authorized to update this content
+        if content.created_by != user.id:
+            return handle_error("You do not have permission to edit this content.")
         
-        # Query the content by ID
-        current_quote = db.query(Content).filter(Content.id == content_id).first()
-        if not current_quote:
-            render_template("error.html", message="Quote not found.")
+        if content.status == "Inactive":
+            return handle_error("This Quote was not found.")
+        
+        # Get the new quote from the form; if not provided, use the current quote
+        new_quote = request.form.get("quote", "").strip()
+        content.quote = new_quote if new_quote else content.quote
+        content.updated_at = datetime.now(timezone.utc)
 
-        # If the new quote is empty, fall back to the current quote
-        updated_quote = new_quote if new_quote else current_quote.quote
-        update_time = datetime.now(timezone.utc) if datetime.now(timezone.utc) else current_quote.created_at
-
-        db.query(Content).filter(Content.id == content_id).update({"quote": updated_quote, "updated_at": update_time})
         db.commit()
+        print("update content successful")
         return redirect(url_for("index"))
             
     except Exception as e:
         db.rollback()  # Rollback on error
-        return render_template("error.html", message=f"Error: {str(e)}")
+        return handle_error(f"Error: {str(e)}")
     finally:
         db.close()  # Close the session after the operation
 
 @app.route("/content/<content_id>", methods=["DELETE"])
 def delete_content(content_id):
     # User must be logged in to create content
-    user_id = session.get("user_id")
-    if not user_id:
+    user = get_current_user()
+    if not user:
         return redirect(url_for("login"))
     
     db: Session = next(get_db())
     try:
-        user = db.query(User).filter(User.id == user_id).first()
-        if not user:
-            return render_template("error.html", message="User not found.")
+        content = db.query(Content).filter(Content.id == content_id).first()
+        if not content:
+            return handle_error("Content not found.")
         
-        # role need to be authorized
-        role_id = user.role_id
-        role = db.query(Role).filter(Role.id == role_id).first()
-        if not role or role.name != "user" or "delete_own_content" not in role.permissions:
-            return render_template("index.html", error="Unauthorized to delete content.")
+        # Check if the user is authorized to delete this content
+        if content.created_by != user.id:
+            return handle_error("You do not have permission to delete this content.")
+        
+        if content.status == "Inactive":
+            return handle_error("This Quote was not found.")
+        
+        content.status = "Inactive"
+        content.updated_at = datetime.now(timezone.utc)
 
-        db.query(Content).filter(Content.id == content_id).update({"status": "Inactive", "updated_at": datetime.now(timezone.utc)})
         db.commit()
+        print("delete content successful")
         return redirect(url_for("index"))
             
     except Exception as e:
         db.rollback()  # Rollback on error
-        return render_template("error.html", message=f"Error: {str(e)}")
+        return handle_error(f"Error: {str(e)}")
     finally:
         db.close()  # Close the session after the operation
 
 @app.route("/content/ban/<content_id>", methods=["PATCH"])
 def ban_content(content_id):
     # User must be logged in to create content
-    user_id = session.get("user_id")
-    if not user_id:
+    user = get_current_user()
+    if not user:
         return redirect(url_for("login"))
+
+    if not check_permission(user, "ban"):
+        return handle_error("Unauthorized to ban content.")
     
     db: Session = next(get_db())
     try:
-        user = db.query(User).filter(User.id == user_id).first()
-        if not user:
-            return render_template("error.html", message="User not found.")
+        content = db.query(Content).filter(Content.id == content_id).first()
+        if not content:
+            return handle_error("Content not found.")
         
-        # role need to be authorized
-        role_id = user.role_id
-        role = db.query(Role).filter(Role.id == role_id).first()
-        if not role or role.name != "admin" or "ban" not in role.permissions:
-            return render_template("index.html", error="Unauthorized to ban content.")
+        # Check if the content is already inactive
+        if content.status == "Inactive" or content.status == "Ban":
+            return handle_error("This content is already archived or baded and cannot be banned.")
         
-        quote = db.query(Content).filter(Content.id == content_id).first()
-        if not quote:
-            return render_template("error.html", message="Quote not found.")
+        content.status = "Ban" if content.status == "Active" else "Inactive"
+        content.updated_at = datetime.now(timezone.utc)
         
-        if quote.status == "Active":
-            db.query(Content).filter(Content.id == content_id).update({"status": "Ban", "updated_at": datetime.now(timezone.utc)})
-            db.commit()
-        elif quote.status == "Ban":
-            db.query(Content).filter(Content.id == content_id).update({"status": "Inactive", "updated_at": datetime.now(timezone.utc)})
-            db.commit()
+        db.commit()
+        print("ban content successful")
         return redirect(url_for("index"))
             
     except Exception as e:
         db.rollback()  # Rollback on error
-        return render_template("error.html", message=f"Error: {str(e)}")
+        return handle_error(f"Error: {str(e)}")
     finally:
         db.close()  # Close the session after the operation
 
 @app.route("/listusers", methods=["GET"])
 def list_user():
     # User must be logged in to create content
-    user_id = session.get("user_id")
-    if not user_id:
+    user = get_current_user()
+    if not user:
         return redirect(url_for("login"))
+    
+    if not check_permission(user, "updateadmin"):
+        return handle_error("Unauthorized to list users.")
     
     db: Session = next(get_db())
     try:
-        user = db.query(User).filter(User.id == user_id).first()
-        if not user:
-            return render_template("error.html", message="User not found.")
-            
-        # role need to be authorized
-        role_id = user.role_id
-        role = db.query(Role).filter(Role.id == role_id).first()
-        if not role or role.name != "superadmin" or "updateadmin" not in role.permissions:
-            return render_template("index.html", error="Unauthorized to list users.")
-        
         # check target email
         all_users = db.query(User).all()
         users_list = [{"id": user.id, "email": user.email} for user in all_users]
+        print("list user successful")
         return jsonify(users_list)
             
     except Exception as e:
         db.rollback()  # Rollback on error
-        return render_template("error.html", message=f"Error: {str(e)}")
+        return handle_error(f"Error: {str(e)}")
     finally:
         db.close()  # Close the session after the operation
 
 @app.route("/managerole/<target_user_id>", methods=["PATCH"])
 def manage_role(target_user_id):
     # User must be logged in to create content
-    user_id = session.get("user_id")
-    if not user_id:
+    user = get_current_user()
+    if not user:
         return redirect(url_for("login"))
+
+    if not check_permission(user, "updateadmin"):
+        return handle_error("Unauthorized to change user role.")
+
     new_role = request.form.get("role", "").strip()
     if new_role not in ["user", "admin"]:
-        return render_template("error.html", message="Bad Request.")
+        return handle_error("Invalid role.")
     
     db: Session = next(get_db())
     try:
-        user = db.query(User).filter(User.id == user_id).first()
-        if not user:
-            return render_template("error.html", message="User not found.")
-        
-        # role need to be authorized
-        role_id = user.role_id
-        role = db.query(Role).filter(Role.id == role_id).first()
-        if not role or role.name != "superadmin" or "updateadmin" not in role.permissions:
-            return render_template("index.html", error="Unauthorized to list users.")
-        
-        # check user
         target_user = db.query(User).filter(User.id == target_user_id).first()
         if not target_user:
-            return render_template("error.html", message="Target user not found.")
+            return handle_error("User not found.")
+        
+        role = db.query(Role).filter(Role.name == new_role).first()
+        if not role:
+            return handle_error("Role not found.")
 
-        # check target role
-        target_role_id = target_user.role_id
-        target_role = db.query(Role).filter(Role.id == role_id).first()
-        if not target_role or target_role.name not in ["user", "admin"]:
-            return render_template("index.html", error="Failed to give admin authority.")
-        
-        # check new role
-        new_role = db.query(Role).filter(Role.name == new_role).first()
-        new_role_id = new_role.id
-        new_role_name = new_role.name
-        
-        if target_role.name  == "user" and new_role_name == "admin":
-            db.query(User).filter(User.id == target_user_id).update({"role_id": new_role_id})
-            db.commit()
-        elif target_role.name  == "admin" and new_role_name == "user":
-            db.query(User).filter(User.id == target_user_id).update({"role_id": new_role_id})
-            db.commit()
-        else:
-            db.query(User).filter(User.id == target_user_id).update({"role_id": target_role_id})
-            db.commit()
-        
+        target_user.role_id = role.id
+        db.commit()
+        print("manage role successful")
         return redirect(url_for("index"))
             
     except Exception as e:
         db.rollback()  # Rollback on error
-        return render_template("error.html", message=f"Error: {str(e)}")
+        return handle_error(f"Error: {str(e)}")
     finally:
         db.close()  # Close the session after the operation
 
 if __name__ == "__main__":
     # Get the port from the .env file
     port = int(os.getenv("Port", 5000)) # default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
